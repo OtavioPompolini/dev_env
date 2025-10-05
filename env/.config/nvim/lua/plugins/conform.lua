2f0144f56f0d2206378dcc2c48e9ca4f7cc68ea1
Otavio Pompolini <luis_otac_pomp@hotmail.com>
2025-10-04 22:17:46 -0300

    fix nvim conform

--- env/.config/nvim/lua/plugins/conform.lua
+++ env/.config/nvim/lua/plugins/conform.lua
@@ -1,21 +1,21 @@
 return {
-	"stevearc/conform.nvim",
-	event = { "BufWritePre" },
-	cmd = { "ConformInfo" },
-	opts = {
-		notify_on_error = false,
-		format_after_save = {
-			async = true,
-			timeout_ms = 500,
-			lsp_fallback = true,
-		},
-		formatters_by_ft = {
-			javascript = { { "prettierd", "prettier" } },
-			typescript = { { "prettierd", "prettier" } },
-			typescriptreact = { { "prettierd", "prettier" } },
-			lua = { "stylua" },
-			sql = { "sql_formatter" },
-			go = { "goimports-reviser" },
-		},
-	},
+  "stevearc/conform.nvim",
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
+  opts = {
+    notify_on_error = false,
+    format_after_save = {
+      async = true,
+      timeout_ms = 500,
+      lsp_fallback = true,
+    },
+    formatters_by_ft = {
+      javascript = { "prettierd", "prettier" },
+      typescript = { "prettierd", "prettier" },
+      typescriptreact = { "prettierd", "prettier" },
+      lua = { "stylua" },
+      sql = { "sql_formatter" },
+      go = { "goimports-reviser" },
+    },
+  },
 }
